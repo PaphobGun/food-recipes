bea2aab645d0a311a3a38e3018db52050734e2e8
PaphobGun <paphob.a@gmail.com>
2019-04-02 22:16:51 +0700

    Updates

---
+++ precache-manifest.0adcab548f08595532480a5503deb119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf4ba002994098c66e6eea0733d0851",
+    "url": "/food-recipes/static/media/bg-welcome.cdf4ba00.jpg"
+  },
+  {
+    "revision": "0e02eda8c5293b84e940",
+    "url": "/food-recipes/static/js/runtime~main.bf7c1f15.js"
+  },
+  {
+    "revision": "6a73360c343b5d4a60c9",
+    "url": "/food-recipes/static/js/main.f0963e38.chunk.js"
+  },
+  {
+    "revision": "ab0daf4140cb0f34718d",
+    "url": "/food-recipes/static/js/2.53e0cf9e.chunk.js"
+  },
+  {
+    "revision": "6a73360c343b5d4a60c9",
+    "url": "/food-recipes/static/css/main.5c3dca0e.chunk.css"
+  },
+  {
+    "revision": "21eb3ec2af20e5638445a4f30efb6800",
+    "url": "/food-recipes/index.html"
+  }
+];
